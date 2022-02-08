6f49d756b6ce72ac5de0aef59c2c07b2f6a4f661
Dima Brinev <theorion2015@gmail.com>
2022-02-08 19:27:36 +0200

    4 homework

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('UL');
+  for (const friend of friends){
+    const li = document.createElement('LI');
+    li.innerText = `${friend.firstName} ${friend.lastName}`
+    ul.appendChild(li);
+  }
+  return ul;
 }
