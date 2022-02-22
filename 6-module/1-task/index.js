06ea5ba97f0e6493bc68594abf133d530166fdbb
Dima Brinev <theorion2015@gmail.com>
2022-02-22 13:59:06 +0200

    6 homework

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,40 @@
  */
 export default class UserTable {
   constructor(rows) {
+    const table = document.createElement('table');
+    const tHead = document.createElement('thead');
+    let keys = Object.keys(rows[0]);
+    const tr = document.createElement('tr');
+    for (const key of keys) {
+      const th = document.createElement('th');
+      th.innerText = key;
+      tr.appendChild(th);
+    }
+    tr.appendChild(document.createElement('th'));
+    tHead.appendChild(tr);
+    table.appendChild(tHead);
+    const tBody = document.createElement('tbody');
+    for (let row of rows){
+      const tr = document.createElement('tr');
+        for (let dataKey in row) {
+          const td = document.createElement('td');
+          td.innerText = row[dataKey];
+          tr.appendChild(td);
+        }
+        const td = document.createElement('td');
+        const button = document.createElement('button');
+        button.innerText = 'X';
+        td.appendChild(button);
+        tr.appendChild(td);
+        tBody.appendChild(tr);
+    }
+    table.appendChild(tBody);
+    table.addEventListener('click',(e) => {
+      if (e.target.tagName === 'BUTTON'){
+        const tr = e.target.closest('tr');
+        tr.remove();
+      }
+    })
+    this.elem = table; 
   }
 }
