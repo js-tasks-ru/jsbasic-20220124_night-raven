858506174d4ba0de534f7d285c847704473a3d31
Dima Brinev <theorion2015@gmail.com>
2022-02-27 18:30:46 +0200

    7-homework

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,55 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps - 1;
+    this.value = value;
+    this.elem = createElement(`
+    <div class="container" style="padding: 50px;">
+        <div class="slider">
+          <div class="slider__thumb" style="left: 50%;">
+            <span class="slider__value">2</span>
+          </div>
+          <div class="slider__progress" style="width: 50%;"></div>
+          <div class="slider__steps">
+          </div>
+      </div>
+    </div>
+    `);
+    const sliderSteps = this.elem.querySelector('.slider__steps');
+    for (let i = 0; i < steps; i++){
+      const span = document.createElement('span');
+      if (i === value) span.classList.add('slider__step-active');
+      sliderSteps.appendChild(span);
+    }
+    this.sliderThumb = this.elem.querySelector('.slider__thumb');
+    this.sliderValue = this.elem.querySelector('.slider__value');
+    this.sliderProgress = this.elem.querySelector('.slider__progress');
+    this.sliderValue.textContent = value;
+    this.sliderProgress.style.width = `${(value)*(100/this.steps)}`+'%'
+    this.sliderThumb.style.left = `${(value)*(100/this.steps)}`+'%'
+    this.slider = this.elem.querySelector('.slider');
+    this.elem.addEventListener('click',(e)=>{
+        let coords = this.elem.querySelector('.slider').getBoundingClientRect();
+        let offsetWidth = (e.clientX-coords.x);
+        let value = Math.round(offsetWidth/(coords.width/this.steps));
+        if (value !== this.value) {
+          this.value = value;
+          let sliderChangeEvent = new CustomEvent('slider-change', { 
+            detail: this.value, 
+            bubbles: true 
+          })
+          this.elem.dispatchEvent(sliderChangeEvent);
+        }
+        let spans = this.elem.querySelectorAll('span');
+          for (let span of spans){
+            span.classList.remove('slider__step-active');
+          }
+        spans[value].classList.add('.slider__step-active');
+        this.sliderValue.textContent = value;
+        this.sliderProgress.style.width = `${(value)*(100/this.steps)}`+'%'
+        this.sliderThumb.style.left = `${(value)*(100/this.steps)}`+'%'
+    });
   }
+
 }
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,83 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.value = value;
+    let segments = steps - 1;
+    this.elem = createElement(`
+    <div class="container" style="padding: 50px;">
+        <div class="slider">
+          <div class="slider__thumb" style="left: 50%;">
+            <span class="slider__value">2</span>
+          </div>
+          <div class="slider__progress" style="width: 50%;"></div>
+          <div class="slider__steps">
+          </div>
+      </div>
+    </div>
+    `);
+    const sliderSteps = this.elem.querySelector('.slider__steps');
+    for (let i = 0; i < steps; i++){
+      const span = document.createElement('span');
+      if (i === value) span.classList.add('slider__step-active');
+      sliderSteps.appendChild(span);
+    }
+    let slider = this.elem.querySelector('.slider');
+    let sliderThumb = this.elem.querySelector('.slider__thumb');
+    let sliderValue = this.elem.querySelector('.slider__value');
+    let sliderProgress = this.elem.querySelector('.slider__progress');
+
+    sliderValue.textContent = value;
+    sliderThumb.ondragstart = () => false;
+    sliderProgress.style.width = `${(value)*(100/segments)}`+'%'
+    sliderThumb.style.left = `${(value)*(100/segments )}`+'%'
+    
+
+    sliderThumb.addEventListener('pointerdown',(e)=>{
+        let spans = this.elem.querySelectorAll('span');
+        for (let span of spans){
+          span.classList.remove('slider__step-active');
+        }
+        slider.classList.add('slider_dragging');
+       
+        let coords = this.elem.querySelector('.slider').getBoundingClientRect();
+        let offsetWidth = (e.clientX-coords.x);
+        let oldValue = this.value;
+        document.addEventListener('pointermove',pointMove);
+        
+        function pointMove(e) {
+          coords = slider.getBoundingClientRect();
+          offsetWidth = (e.clientX-coords.x);
+          if (offsetWidth <=0 || offsetWidth >= coords.width) return;
+          sliderThumb.style.left = `${offsetWidth/330*100}%`;
+          sliderProgress.style.width = `${offsetWidth/330*100}%`;
+          let value = Math.round(offsetWidth/(coords.width/segments ));
+          sliderValue.textContent = value;
+        };
+
+        this.elem.addEventListener('pointerup',(e) => {
+          document.removeEventListener('pointermove',pointMove)
+          slider.classList.remove('slider_dragging');
+          coords = slider.getBoundingClientRect();
+          offsetWidth = (e.clientX-coords.x);
+          if (offsetWidth <=0 || offsetWidth >= coords.width) return;
+          let value = Math.round(offsetWidth/(coords.width/segments ));
+          sliderProgress.style.width = `${(value)*(100/segments)}`+'%';
+          sliderThumb.style.left = `${(value)*(100/segments)}`+'%';
+          sliderValue.textContent = value;
+          if (value !== oldValue) {
+            oldValue = value;
+            for (let span of spans){
+              span.classList.remove('slider__step-active');
+            }
+            spans[value].classList.add('.slider__step-active');
+            let sliderChangeEvent = new CustomEvent('slider-change', { 
+              detail: this.value, 
+              bubbles: true 
+              })
+            slider.dispatchEvent(sliderChangeEvent);
+        }});
+  
+    });
   }
 }
